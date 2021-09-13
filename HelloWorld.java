69f31218859126fe78f16c71292bc10ca839f689
Chunduru Mounika1 <chunduru.mounika1@ibm.com>
2021-09-13 11:19:26 +0530

    first commit of java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello this is java pgm");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("value of i "+i);
+        }
+    }
+}
